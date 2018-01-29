f41477208930d4d16aa2f7f9de1a8a96db242f24
sofaw <soph.wood52@gmail.com>
2018-01-29 13:15:58 +0000

    Initial commit.

---
+++ soar.assessment.Y3853992/src/soar/assessment/Y3853992/Customer.java
@@ -0,0 +1,56 @@
+package soar.assessment.Y3853992;
+
+public class Customer {
+	private String id;
+	private String fullname;
+	private String email;
+	private String username;
+	private String password;
+	
+	public Customer(String id, String fullname, String email, String username) {
+		this.id = id;
+		this.fullname = fullname;
+		this.email = email;
+		this.username = username;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getFullname() {
+		return fullname;
+	}
+	
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ soar.assessment.Y3853992/src/soar/assessment/Y3853992/Customers.java
@@ -0,0 +1,7 @@
+package soar.assessment.Y3853992;
+
+public class Customers {
+	public void createCustomer(Customers details) {
+		
+	}
+}
