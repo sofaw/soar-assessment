16bcd815be8bbe96a5609c4931b47745039ebfb6
sofaw <soph.wood52@gmail.com>
2018-02-02 19:30:24 +0000

    Add index.jsp and h2 database.

--- soar.assessment.Y3853992/src/soar/assessment/Y3853992/Customers.java
+++ soar.assessment.Y3853992/src/soar/assessment/Y3853992/Customers.java
@@ -5,38 +5,7 @@ import java.sql.*;
 public class Customers {
 	public Customers() {};
 	
-	public void createCustomer(Customer details) throws ClassNotFoundException, SQLException {
-		Class.forName("org.h2.Driver");
-		Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
-
-		// Create table
-	    String query = "CREATE TABLE CUSTOMERS ("
-	    		+ "id INT NOT NULL, "
-	    		+ "fullname VARCHAR(20) NOT NULL, "
-	    		+ "email VARCHAR(20) NOT NULL, "
-	    		+ "username VARCHAR(20) NOT NULL, "
-	    		+ "password VARCHAR(20) NOT NULL"
-	    		+ ");";
-		Statement stmt = conn.createStatement();
-		stmt.execute(query);
-		
-		// Add entries to table
-		stmt = conn.createStatement();
-		query = "INSERT INTO CUSTOMERS VALUES"
-				+ "("
-				+ details.getId() + ", " 
-				+ details.getFullname() + ", "
-				+ details.getEmail() + ", "
-				+ details.getUsername() + ", "
-				+ details.getPassword() + ");";
-		stmt.execute(query);
-		
-		conn.close();
-		
-		// TODO: change to try/catch and add faults
-	}
-	
-	public float testConnection(float a, float b) {
-		return a + b;
+	public void createCustomer(Customer details) {
+		// TODO
 	}
 }
