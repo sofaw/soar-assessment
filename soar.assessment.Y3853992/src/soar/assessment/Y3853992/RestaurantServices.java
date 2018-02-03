96bf4361d71e95771acb242f71ccad74cd5073c4
sofaw <soph.wood52@gmail.com>
2018-02-03 19:38:00 +0000

    Add customer service.

--- soar.assessment.Y3853992/src/soar/assessment/Y3853992/RestaurantServices.java
+++ soar.assessment.Y3853992/src/soar/assessment/Y3853992/RestaurantServices.java
@@ -38,4 +38,37 @@ public class RestaurantServices {
         
     	return restaurantID;
 	}
+	
+	public void addMenuItem(int restaurantID, String title, float price) throws ClassNotFoundException, SQLException {
+		Class.forName("org.h2.Driver");
+		Connection con = DriverManager.getConnection("jdbc:h2:~/test", "sa", "sa" );
+        Statement stmt = con.createStatement();
+        
+		stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ITEMS("
+        		+ "ITEM_ID INT PRIMARY KEY AUTO_INCREMENT, "
+        		+ "RESTAURANT_ID  INT, "
+        		+ "TITLE VARCHAR(35), "
+        		+ "PRICE FLOAT, "
+        		+ "foreign key (RESTAURANT_ID) references RESTAURANTS(RESTAURANT_ID))");
+
+        stmt.executeUpdate("INSERT INTO ITEMS (RESTAURANT_ID, TITLE, PRICE) VALUES ("
+        		+ "\'" + restaurantID + "\', "
+        		+ "\'" + title + "\', "
+        		+ "\'" + price + "\')" );
+	}
+	
+	public void deleteMenu(int restaurantID) throws ClassNotFoundException, SQLException {
+		Class.forName("org.h2.Driver");
+		Connection con = DriverManager.getConnection("jdbc:h2:~/test", "sa", "sa" );
+        Statement stmt = con.createStatement();
+		
+		stmt.executeUpdate("DELETE FROM ITEMS WHERE RESTAURANT_ID=" + restaurantID);
+	}
+	
+	// TODO
+	/*public void changeOrderStatus(int orderID, String status) throws ClassNotFoundException, SQLException {
+		Class.forName("org.h2.Driver");
+		Connection con = DriverManager.getConnection("jdbc:h2:~/test", "sa", "sa" );
+        Statement stmt = con.createStatement();
+	}*/
 }
